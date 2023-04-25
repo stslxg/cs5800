e7add65df30a0cd8f197ec576f3476dd256e96a6
Xuangui Huang <stslxg@gmail.com>
2023-04-25 14:44:34 -0400

    use gtest

--- hash/hashtest.cpp
+++ hash/hashtest.cpp
@@ -1,2 +1,19 @@
+#include <gtest/gtest.h>
 #include "hash.hpp"
 
+TEST(TestHash, TestInsert) {
+    Hash hash_table(1000);
+    hash_table.insert("hello", 1);
+    hash_table.insert("world", 1);
+    hash_table.insert("hello", 1);
+    hash_table.insert("world", 1);
+
+    EXPECT_EQ(hash_table.find("hello"), 2);
+    EXPECT_EQ(hash_table.find("world"), 2);
+    EXPECT_EQ(hash_table.find("helloworld"), 0);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
