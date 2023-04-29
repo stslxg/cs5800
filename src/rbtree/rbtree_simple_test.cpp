26e361ba9f3d2a51a95143acc298ae3e5287d324
Xuangui Huang <stslxg@gmail.com>
2023-04-29 22:12:29 +0000

    add more rbtree test

--- src/rbtree/rbtree_simple_test.cpp
+++ src/rbtree/rbtree_simple_test.cpp
@@ -47,6 +47,31 @@ TEST(TestRBTreeSimple, TestInsertWithTwoNodes) {
     EXPECT_EQ(2, tree.get_depth());
 }
 
+TEST(TestRBTreeSimple, TestInsertWithThreeNodes) {
+    Tree tree;
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(3);
+    ASSERT_NE(nullptr, tree.min());
+    ASSERT_NE(nullptr, tree.max());
+    EXPECT_EQ(1, tree.min()->val);
+    EXPECT_EQ(3, tree.max()->val);
+    ASSERT_NE(nullptr, tree.search(1));
+    EXPECT_EQ(1, tree.search(1)->val);
+    ASSERT_NE(nullptr, tree.search(2));
+    EXPECT_EQ(2, tree.search(2)->val);
+    ASSERT_NE(nullptr, tree.search(3));
+    EXPECT_EQ(3, tree.search(3)->val);
+    EXPECT_EQ(tree.search(2), tree.pred(tree.search(3)));
+    EXPECT_EQ(tree.search(2), tree.suc(tree.search(1)));
+    EXPECT_EQ(tree.search(1), tree.pred(tree.search(2)));
+    EXPECT_EQ(tree.search(3), tree.suc(tree.search(2)));
+    EXPECT_EQ(2, tree.get_depth());
+    EXPECT_EQ(tree.search(1), tree.search(2)->left);
+    EXPECT_EQ(tree.search(3), tree.search(2)->right);
+}
+
+
 TEST(TestRBTreeSimple, TestDeleteWithOneNode) {
     Tree tree;
     tree.insert(1);
@@ -72,6 +97,23 @@ TEST(TestRBTreeSimple, TestDeleteWithTwoNodes) {
     EXPECT_EQ(nullptr, tree.pred(tree.search(2)));
     EXPECT_EQ(nullptr, tree.suc(tree.search(2)));
     EXPECT_EQ(1, tree.get_depth());
+}
+
+TEST(TestRBTreeSimple, TestDeleteWithThreeNodes) {
+    Tree tree;
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(3);
+    tree.del(tree.search(2));
+    ASSERT_NE(nullptr, tree.min());
+    ASSERT_NE(nullptr, tree.max());
+    EXPECT_EQ(1, tree.min()->val);
+    EXPECT_EQ(3, tree.max()->val);
+    ASSERT_NE(nullptr, tree.search(1));
+    EXPECT_EQ(1, tree.search(1)->val);
+    ASSERT_NE(nullptr, tree.search(3));
+    EXPECT_EQ(3, tree.search(3)->val);
+    ASSERT_EQ(nullptr, tree.search(2));
 }
 
 TEST(TestRBTreeSimple, TestDeleteBothTwoNodes) {
