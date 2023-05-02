86870f5937f3737ec8a903045054e2793b8b5832
Xuangui Huang <stslxg@gmail.com>
2023-05-02 18:27:00 -0400

    add more simple tests for skip list

--- tests/skiplist/skiplist_simple_test.cpp
+++ tests/skiplist/skiplist_simple_test.cpp
@@ -6,3 +6,42 @@ TEST(TestSkipListSimple, TestEmpty) {
     EXPECT_FALSE(L.lookup(0));
 }
 
+TEST(TestSkipListSimple, TestInsert) {
+    SkipList L;
+    L.insert(0);
+    EXPECT_TRUE(L.lookup(0));
+}
+
+TEST(TestSkipListSimple, TestInsertTwoKeys) {
+    SkipList L;
+    L.insert(0);
+    L.insert(1);
+
+    EXPECT_TRUE(L.lookup(0));
+    EXPECT_TRUE(L.lookup(1));
+
+    EXPECT_EQ(L.search(1), L.search(0)->next[0]);
+}
+
+TEST(TestSkipListSimple, TestDelete) {
+    SkipList L;
+    L.insert(0);
+    EXPECT_TRUE(L.lookup(0));
+    EXPECT_TRUE(L.del(0));
+    EXPECT_FALSE(L.lookup(0));
+}
+
+TEST(TestSkipListSimple, TestDeleteEmpty) {
+    SkipList L;
+    EXPECT_FALSE(L.del(0));
+}
+
+TEST(TestSkipListSimple, TestInsertDelete) {
+    SkipList L;
+    L.insert(0);
+    EXPECT_TRUE(L.lookup(0));
+    EXPECT_TRUE(L.del(0));
+    EXPECT_FALSE(L.lookup(0));
+    L.insert(0);
+    EXPECT_TRUE(L.lookup(0));
+}
